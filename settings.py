1b67dc2f53287b3389fdcca7fc41e627980cc7c2
Gition <ion.cojoc@gmail.com>
2025-05-08 16:24:36 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
-
+"""
 FLASK_APP = os.environ["FLASK_APP"]
 FLASK_ENV = os.environ["FLASK_ENV"]
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -7,6 +7,14 @@ DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
+"""
+FLASK_APP = os.getenv("FLASK_APP", "wsgi.py")
+FLASK_ENV = os.getenv("FLASK_ENV", "production")
+SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+
+DB_USERNAME = os.getenv("DB_USERNAME", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST", "localhost")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = (
     f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:5432/{DATABASE_NAME}"
